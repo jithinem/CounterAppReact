c88a606fe099d4e11be2cbda774267342bb895cf
jithinem <jithinem4007@gmail.com>
2023-04-01 20:07:25 +0530

    counter app

---
+++ src/components/Counter.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+function Counter() {
+    const [counter,setCounter]=useState(0)
+    function increment(){
+      setCounter(counter+1)
+    }
+    function decrement(){
+        if (counter!==0) {
+            setCounter(counter-1)  
+        }
+      }
+      function reset(){
+        setCounter(0)
+      }    
+  
+  return (
+    <div className="App">
+    <header className="App-header">
+       <nav class="navbar navbar-light bg-warning">
+         <div class="container"><h1>Counter Application</h1>
+         <a class="navbar-brand" href="#">
+         </a>
+         </div>
+       </nav>
+       <div style={{
+       backgroundColor:'pink',
+       flexDirection:'column',
+       textAlign:'center',
+       justifyContent:'center',
+       border:'1px solid black',
+       padding:'200px',
+       margin:'auto'
+    }} >
+      <h3>Counter Application</h3>
+      <h1>{counter}</h1>
+      <button style={{backgroundColor:'green',color:'white',padding:'5px',margin:'5px'}} onClick={increment}>Add</button>
+      <button style={{backgroundColor:'gray',color:'white',padding:'5px',margin:'5px'}} onClick={decrement}>Substract</button>
+      <button style={{backgroundColor:'red',color:'white',padding:'5px',margin:'5px'}} onClick={reset}>Reset</button>
+
+    </div>
+    </header>
+  </div>
+
+
+  )
+}
+
+export default Counter
